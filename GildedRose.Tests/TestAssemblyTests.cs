d70307c7572a05becaa06567aa923afda6bb961e
Philip Flyvholm <philipflyvholm@gmail.com>
2021-10-25 16:10:33 +0200

    done
    Co-authored-by: Trindkr <Trindkr@users.noreply.github.com>
    Co-authored-by: Jonas Lindvig <surtshow@users.noreply.github.com>

--- GildedRose.Tests/TestAssemblyTests.cs
+++ GildedRose.Tests/TestAssemblyTests.cs
@@ -1,5 +1,4 @@
 ﻿using Xunit;
-using GildedRose;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System;
@@ -8,40 +7,64 @@ namespace GildedRose.Tests
 {
     public class TestAssemblyTests
     {
-        Program program;   
+        Program program;
         ReadOnlyCollection<Item> _items = new List<Item>{
             new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
-            new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
+            new AgedBrie {Name = "Aged Brie", SellIn = 2, Quality = 0},
             new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
-            new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
-            new Item{ Name = "Backstage passes to a TAFKAL80ETC concert",SellIn = 15,Quality = 20},
-            new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6},
-            new Item {Name = "Test product", SellIn = 1, Quality = 0}
+            new Sulfuras {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+            new Sulfuras { Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80 },
+            new BackstagePass { Name = "Backstage passes to a TAFKAL80ETC concert",SellIn = 15,Quality = 20},
+            new BackstagePass { Name = "Backstage passes to a TAFKAL80ETC concert",SellIn = -5,Quality = 20},
+            new BackstagePass { Name = "Backstage passes to a TAFKAL80ETC concert",SellIn = 10,Quality = 20},
+            new BackstagePass { Name = "Backstage passes to a TAFKAL80ETC concert",SellIn = 5,Quality = 20},
+            new Conjured {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6},
+            new Item {Name = "Test product", SellIn = 1, Quality = 0},
+            new Item {Name = "Test product 2", SellIn = 0, Quality = 20}
         }.AsReadOnly();
-        
 
-        public TestAssemblyTests(){
-            program = new Program(){
-                Items = new List<Item>(_items.Count)
+
+        public TestAssemblyTests()
+        {
+            program = new Program()
+            {
+                Items = new List<Item>{
+                    new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+                    new AgedBrie {Name = "Aged Brie", SellIn = 2, Quality = 0},
+                    new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+                    new Sulfuras {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+                    new Sulfuras { Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80 },
+                    new BackstagePass { Name = "Backstage passes to a TAFKAL80ETC concert",SellIn = 15,Quality = 20},
+                    new BackstagePass { Name = "Backstage passes to a TAFKAL80ETC concert",SellIn = -5,Quality = 20},
+                    new BackstagePass { Name = "Backstage passes to a TAFKAL80ETC concert",SellIn = 10,Quality = 20},
+                    new BackstagePass { Name = "Backstage passes to a TAFKAL80ETC concert",SellIn = 5,Quality = 20},
+                    new Conjured {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6},
+                    new Item {Name = "Test product", SellIn = 1, Quality = 0},
+                    new Item {Name = "Test product 2", SellIn = 0, Quality = 20}
+                }
             };
-            foreach (var item in _items){
-                program.Items.Add(new Item{Name = item.Name, SellIn = item.SellIn, Quality = item.Quality});
-            }
             program.UpdateQuality();
-            System.Console.WriteLine("ET ELLER ANDET");
         }
-        
+
+        [Fact]
+        public void Run_Main()
+        {
+            Program.Main(new string[] { "" });
+
+        }
+
         [Fact]
         public void never_more_than_50()
         {
             for (var i = 0; i < program.Items.Count; i++)
             {
-                if(_items[i].Quality < 50){
+                if (_items[i].Quality < 50)
+                {
                     Assert.True(program.Items[i].Quality <= 50);
                 }
-            }    
+            }
         }
-        
+
         [Fact]
         public void never_negative()
         {
@@ -56,9 +79,121 @@ namespace GildedRose.Tests
         {
             for (var i = 0; i < program.Items.Count; i++)
             {
-                if(_items[i].SellIn > 0){ 
+                if (!program.Items[i].Name.Contains("Sulfuras"))
+                {
                     var expected = _items[i].SellIn - 1;
                     Assert.Equal(expected, program.Items[i].SellIn);
+                }
+            }
+        }
+
+        [Fact]
+        public void sulfuras_does_not_lower_in_sell_date_and_quality()
+        {
+            for (var i = 0; i < program.Items.Count; i++)
+            {
+                if (program.Items[i].Name.Contains("Sulfuras"))
+                {
+                    var expectedSellin = _items[i].SellIn;
+                    Assert.Equal(expectedSellin, program.Items[i].SellIn);
+                    var expectedQuality = _items[i].Quality;
+                    Assert.Equal(expectedQuality, program.Items[i].Quality);
+                }
+            }
+        }
+
+        [Fact]
+        public void quality_decreases_unless_special_or_passed_sellin()
+        {
+            for (var i = 0; i < program.Items.Count; i++)
+            {
+
+                var item = program.Items[i];
+                if (!item.Name.Contains("Sulfuras") && !item.Name.Contains("Backstage pass") && !item.Name.Contains("Aged Brie") && _items[i].Quality > 0 && item.SellIn >= 0)
+                {
+                    var expectedQuality = _items[i].Quality - 1;
+                    if(item.Name.Contains("Conjured")){
+                        expectedQuality--;
+                    }
+                    Assert.Equal(expectedQuality, item.Quality);
+                }
+            }
+        }
+
+        [Fact]
+        public void quality_decreases_faster_unless_special_if_passed_sellin()
+        {
+            for (var i = 0; i < program.Items.Count; i++)
+            {
+
+                var item = program.Items[i];
+                if (!item.Name.Contains("Sulfuras") && !item.Name.Contains("Backstage pass") && !item.Name.Contains("Aged Brie") && _items[i].Quality > 0 && item.SellIn < 0)
+                {
+                    var expectedQuality = _items[i].Quality - 2;
+                    Assert.Equal(expectedQuality, item.Quality);
+                }
+            }
+        }
+
+        [Fact]
+        public void Backstage_passes_increase()
+        {
+            for (var i = 0; i < program.Items.Count; i++)
+            {
+
+                var item = program.Items[i];
+                if (item.Name.Contains("Backstage pass") && _items[i].Quality < 50)
+                {
+                    if (item.SellIn > 10)
+                    {
+                        //+11 days
+
+                        var expectedQuality = _items[i].Quality + 1;
+                        Assert.Equal(expectedQuality, item.Quality);
+                    }
+                    else if (item.SellIn > 5)
+                    {
+                        //6-10 days
+
+                        var expectedQuality = _items[i].Quality + 2;
+                        Assert.Equal(expectedQuality, item.Quality);
+                    }
+                    else if (item.SellIn > 0)
+                    {
+                        //0-5 days
+
+                        var expectedQuality = _items[i].Quality + 3;
+                        Assert.Equal(expectedQuality, item.Quality);
+                    }
+                    else
+                    {
+
+                        //-1 days
+                        var expectedQuality = 0;
+                        Assert.Equal(expectedQuality, item.Quality);
+                    }
+                }
+            }
+        }
+
+        [Fact]
+        public void Aged_Brei_increase()
+        {
+            for (var i = 0; i < program.Items.Count; i++)
+            {
+                var item = program.Items[i];
+                if (item.Name.Contains("Aged Brie"))
+                {
+                    if (_items[i].Quality != 50)
+                    {
+                        var expectedQuality = _items[i].Quality + 1;
+                        Assert.Equal(expectedQuality, item.Quality);
+                    }
+                    else
+                    {
+                        var expectedQuality = _items[i].Quality;
+                        Assert.Equal(expectedQuality, item.Quality);
+                    }
                 }
             }
         }
--- GildedRose/Program.cs
+++ GildedRose/Program.cs
@@ -6,7 +6,10 @@ namespace GildedRose
     public class Program
     {
         public IList<Item> Items;
-        static void Main(string[] args)
+
+        public static int maxQuality = 50;
+        public static int minQuality = 0;
+        public static void Main(string[] args)
         {
             System.Console.WriteLine("OMGHAI!");
 
@@ -15,30 +18,30 @@ namespace GildedRose
                 Items = new List<Item>
                                           {
                 new Item { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20 },
-                new Item { Name = "Aged Brie", SellIn = 2, Quality = 0 },
+                new AgedBrie { Name = "Aged Brie", SellIn = 2, Quality = 0 },
                 new Item { Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7 },
-                new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80 },
-                new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80 },
-                new Item
+                new Sulfuras { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80 },
+                new Sulfuras { Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80 },
+                new BackstagePass
                 {
                     Name = "Backstage passes to a TAFKAL80ETC concert",
                     SellIn = 15,
                     Quality = 20
                 },
-                new Item
+                new BackstagePass
                 {
                     Name = "Backstage passes to a TAFKAL80ETC concert",
                     SellIn = 10,
                     Quality = 49
                 },
-                new Item
+                new BackstagePass
                 {
                     Name = "Backstage passes to a TAFKAL80ETC concert",
                     SellIn = 5,
                     Quality = 49
                 },
 				// this conjured item does not work properly yet
-				new Item { Name = "Conjured Mana Cake", SellIn = 3, Quality = 6 }
+				new Conjured { Name = "Conjured Mana Cake", SellIn = 3, Quality = 6 }
                                           }
 
             };
@@ -56,83 +59,14 @@ namespace GildedRose
 
         }
 
-
         public void UpdateQuality()
         {
             for (var i = 0; i < Items.Count; i++)
             {
-                if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                {
-                    if (Items[i].Quality > 0)
-                    {
-                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            Items[i].Quality = Items[i].Quality - 1;
-                        }
-                    }
-                }
-                else
-                {
-                    if (Items[i].Quality < 50)
-                    {
-                        Items[i].Quality = Items[i].Quality + 1;
-
-                        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (Items[i].SellIn < 11)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-
-                            if (Items[i].SellIn < 6)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-                        }
-                    }
-                }
-
-                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                {
-                    Items[i].SellIn = Items[i].SellIn - 1;
-                }
-
-                if (Items[i].SellIn < 0)
-                {
-                    if (Items[i].Name != "Aged Brie")
-                    {
-                        if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (Items[i].Quality > 0)
-                            {
-                                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                                {
-                                    Items[i].Quality = Items[i].Quality - 1;
-                                }
-                            }
-                        }
-                        else
-                        {
-                            Items[i].Quality = Items[i].Quality - Items[i].Quality;
-                        }
-                    }
-                    else
-                    {
-                        if (Items[i].Quality < 50)
-                        {
-                            Items[i].Quality = Items[i].Quality + 1;
-                        }
-                    }
-                }
+                var item = Items[i];
+                item.update();
             }
         }
-
     }
 
     public class Item
@@ -142,6 +76,104 @@ namespace GildedRose
         public int SellIn { get; set; }
 
         public int Quality { get; set; }
+
+        public virtual void update()
+        {
+            SellIn--;
+            if (Quality <= Program.minQuality){
+                 return;
+            }
+            if (Quality > Program.maxQuality)
+            {
+                Quality = Program.maxQuality;
+            }
+            if (SellIn >= 0)
+            {
+                Quality -= 1;
+            }
+            else
+            {
+                Quality -= 2;
+            }
+        }
+    }
+    public class AgedBrie : Item
+    {
+        public override void update()
+        {
+            SellIn--;
+            if (Quality < Program.minQuality){
+                Quality = 0;
+                 return;
+            }
+            if (Quality > Program.maxQuality)
+            {
+                Quality = Program.maxQuality;
+            }
+
+            Quality += 1;
+        }
+    }
+    public class BackstagePass : Item
+    {
+        public override void update()
+        {
+            SellIn--;
+            if (Quality <= Program.minQuality){
+                 return;
+            }
+            if (Quality > Program.maxQuality)
+            {
+                Quality = Program.maxQuality;
+            }
+            if (SellIn > 10)
+            {
+                Quality += 1;
+            }
+            else if (SellIn > 5)
+            {
+                Quality += 2;
+            }
+            else if (SellIn > 0)
+            {
+                Quality += 3;
+            }
+            else
+            {
+                Quality = 0;
+            }
+        }
+    }
+    public class Sulfuras : Item
+    {
+        public override void update()
+        {
+            //Do nothing   
+        }
+    }
+
+    public class Conjured : Item
+    {
+        public override void update()
+        {
+            SellIn--;
+            if (Quality <= Program.minQuality){
+                 return;
+            }
+            if (Quality > Program.maxQuality)
+            {
+                Quality = Program.maxQuality;
+            }
+            if (SellIn >= 0)
+            {
+                Quality -= 2;
+            }
+            else
+            {
+                Quality -= 4;
+            }
+            
+        }
     }
 
 }
